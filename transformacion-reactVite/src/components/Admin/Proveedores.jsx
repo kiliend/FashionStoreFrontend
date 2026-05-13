0867ab903b5fc11e129cf1c02b093e44e926fbfc
Kevin Huahuaccapa <huahuaccapa1119@gmail.com>
2026-05-13 03:01:05 -0500

    Actualizar módulo de proveedores

--- transformacion-reactVite/src/components/Admin/Proveedores.jsx
+++ transformacion-reactVite/src/components/Admin/Proveedores.jsx
@@ -1,3 +1,4 @@
+//src\components\Admin\Proveedores.jsx
 import React, { useState, useEffect } from 'react';
 import { getProveedores, setProveedores, getOrdenesCompra, setOrdenesCompra, getProductos, setProductos } from '../../lib/storage';
 
