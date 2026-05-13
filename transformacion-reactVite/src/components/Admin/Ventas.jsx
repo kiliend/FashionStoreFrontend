3ea87e32b1d1c216b3700e8bd29839728627c92e
Kevin Huahuaccapa <huahuaccapa1119@gmail.com>
2026-05-13 03:01:11 -0500

    Actualizar módulo de ventas

--- transformacion-reactVite/src/components/Admin/Ventas.jsx
+++ transformacion-reactVite/src/components/Admin/Ventas.jsx
@@ -1,3 +1,4 @@
+//src\components\Admin\Ventas.jsx
 import React, { useState, useEffect } from 'react';
 import { getProductos, getVentas, setVentas, setProductos } from '../../lib/storage';
 import { useAuth } from '../../contexts/AuthContext';
